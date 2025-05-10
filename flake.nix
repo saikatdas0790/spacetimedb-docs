7837fe9b3e9ac9dce30908139a20635e5dd4dae4
Saikat Das <saikatdas0790@gmail.com>
2025-05-10 04:28:52 +0000

    Comment out spacetimedb package in flake.nix buildInputs

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
           pkgs.mkShell {
             buildInputs = [
               pkgs.rustup
-              pkgs.spacetimedb
+              # pkgs.spacetimedb
             ];
           };
       };
